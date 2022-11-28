46032dda89ca036edbd812e7252c3056606692df
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-28 11:03:57 +0100

    préparation et structure des pages

---
+++ header.php
@@ -0,0 +1,10 @@
+    <header>
+        <div class="nav">
+            <div class="logo"><a href=""></a></div>
+            <div class="nav_bar">
+                <a href="index.php">Acceil</a>
+                <a href="connecxion.php">Se connecter</a>
+                <a href="inscripion.php">Créer compte</a>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Module connexion</title>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+<?php include 'footer.php' ?>
+</body>
+</html>

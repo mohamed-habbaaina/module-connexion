33f4f4be637a9263fff6652feb31d897da1c7386
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-03 02:07:55 +0100

    changement de la photo de profil

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,3 @@
     <footer>
-        <div class="nav">
-            <div class="logo"><a href=""></a></div>
-            <div><a href="https://github.com/mohamed-habbaaina/module-connexion">Lien Github</a></div>
-            <div>Ressources</div>
-            <div>About</div>
-            <div class="nav_bar" class="footer">
-                <a href="index.php">Acceil</a>
-                <a href="connecxion.php">Se connecter</a>
-                <a href="inscripion.php">Créer compte</a>
-            </div>
-        </div>
+            <button class="github"><a href="https://github.com/mohamed-habbaaina/module-connexion">Lien Github</a></button>
     </footer>
